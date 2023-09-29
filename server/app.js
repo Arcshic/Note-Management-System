52b07a13d9c4a7253c3beb975bef2ea54070a4ee
Arcshic <20130421042jyl@gmail.com>
2023-09-29 20:13:44 +1300

    basic server build

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const multer = require("multer");
+const app = express();
+const port = 8080;
+
+
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","*");
+    res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
+    if(req.method == "OPTIONS") res.sendStatus(200);
+    else next();
+})
+
+app.use(express.json())
+
+const update = multer({
+    dest:"./public/upload/temp"
+})
+app.use(update.any())
+
+
+app.get("/",(req,res)=>{
+    res.send("Hello World!")
+})
+app.listen(port,()=>{
+    console.log(`Server has runned on port ${port}`)
+
+})
