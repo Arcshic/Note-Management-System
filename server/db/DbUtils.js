10462df08025eab7d2755f3e694153b2e1c54ed3
Arcshic <20130421042jyl@gmail.com>
2023-10-05 22:48:50 +1300

    package the method by Promise

--- server/db/DbUtils.js
+++ server/db/DbUtils.js
@@ -2,9 +2,27 @@ const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
 const GenId = require("../utils/SnowFlake");
 
+
 const db = new sqlite3.Database(path.join(__dirname,"note-database.db"));
 const genId = new GenId({WorkerId:1});
 
+db.async = {}
+
+db.async.all = (sql,params) =>{
+    return new Promise((resolve,reject)=>{
+        db.all(sql,params,(err,rows)=>{
+            resolve({err,rows})
+        })
+    })
+}
+
+db.async.run = (sql,params) =>{
+    return new Promise((resolve,reject)=>{
+        db.all(sql,params,(err,rows)=>{
+            resolve({err,rows})
+        })
+    })
+}
 module.exports = {
     db,
     genId
