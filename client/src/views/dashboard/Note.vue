664fc901dec8c563cafe9ecde815f0fb99f783af
Arcshic <20130421042jyl@gmail.com>
2023-10-09 03:59:36 +1300

    Add update page for Note

--- client/src/views/dashboard/Note.vue
+++ client/src/views/dashboard/Note.vue
@@ -1,41 +1,59 @@
 <template>
-    
-    <n-tabs default-value="add" justify-content="start" type="line">
-            <n-tab-pane name="list" tab="Note list">
-                <div v-for="(note,index) in noteList">
-                    <n-card :title="note.title">
-                        {{ note.content }}
-                        <template #footer>
-                            <n-space align="center">
-                                <div>Create time: {{ note.create_time }}</div>
-                                <n-button>Update</n-button>
-                                <n-button>Delete</n-button>
-                            </n-space>
-                        </template>
-                    </n-card>
+    <n-tabs default-value="list" justify-content="start" type="line">
+        <n-tab-pane name="list" tab="Note list">
+            <div v-for="(note, index) in noteList">
+                <n-card :title="note.title">
+                    {{ note.content }}
+                    <template #footer>
+                        <n-space align="center">
+                            <div>Create time: {{ note.create_time }}</div>
+                            <n-button>Update</n-button>
+                            <n-button>Delete</n-button>
+                        </n-space>
+                    </template>
+                </n-card>
+            </div>
+            <n-space>
+                <div @click="toPage(pageNum)" v-for="pageNum in pageInfo.pageCount">
+                    <div :style="'color:'+(pageNum == pageInfo.page?'blue':'')">{{ pageNum }}</div>
+                    
                 </div>
-            </n-tab-pane>
-            <n-tab-pane name="add" tab="Add a new note">
-                <n-form>
-                    <n-form-item label="Title">
-                        <n-input v-model:value="addNote.title" placeholder="Please enter a title"/>
-                    </n-form-item>
-                    <n-form-item label="Category">
-                        <n-select v-model:value="addNote.categoryId" :options="categoryOptions" />
-                    </n-form-item>
-                    <n-form-item label="Content">
-                        <RichTextEditor v-model="addNote.content"></RichTextEditor>
-                    </n-form-item>
-                    <n-form-item label="">
-                        <n-button @click="add">Submit</n-button>
-                    </n-form-item>
-                </n-form>
-            </n-tab-pane>
-            <n-tab-pane name="jay chou" tab="周杰伦">
-                七里香
-            </n-tab-pane>
-        </n-tabs>
-    
+            </n-space>
+
+        </n-tab-pane>
+        <n-tab-pane name="add" tab="Add a new note">
+            <n-form>
+                <n-form-item label="Title">
+                    <n-input v-model:value="addNote.title" placeholder="Please enter a title" />
+                </n-form-item>
+                <n-form-item label="Category">
+                    <n-select v-model:value="addNote.categoryId" :options="categoryOptions" />
+                </n-form-item>
+                <n-form-item label="Content">
+                    <RichTextEditor v-model="addNote.content"></RichTextEditor>
+                </n-form-item>
+                <n-form-item label="">
+                    <n-button @click="add">Submit</n-button>
+                </n-form-item>
+            </n-form>
+        </n-tab-pane>
+        <n-tab-pane name="update" tab="Update a Note">
+            <n-form>
+                <n-form-item label="Title">
+                    <n-input v-model:value="updateNote.title" placeholder="Please enter a title" />
+                </n-form-item>
+                <n-form-item label="Category">
+                    <n-select v-model:value="updateNote.categoryId" :options="categoryOptions" />
+                </n-form-item>
+                <n-form-item label="Content">
+                    <RichTextEditor v-model="updateNote.content"></RichTextEditor>
+                </n-form-item>
+                <n-form-item label="">
+                    <n-button @click="update">Submit</n-button>
+                </n-form-item>
+            </n-form>
+        </n-tab-pane>
+    </n-tabs>
 </template>
 
 <script setup>
@@ -57,36 +75,49 @@ const notification = inject("notification")
 const dialog = inject("dialog")
 
 const addNote = reactive({
-    categoryId:0,
-    title:"",
-    content:"Please enter the content"
+    categoryId: 0,
+    title: "",
+    content: "Please enter the content"
+})
+const updateNote = reactive({
+    id:0,
+    categoryId: 0,
+    title: "",
+    content: "Please enter the content"
 })
 
 const categoryOptions = ref([])
 const noteList = ref([])
-
-onMounted(()=>{
+let pageInfo = reactive({
+    page: 1,
+    pageSize: 3,
+    pageCount: 0,
+    Count: 0
+})
+onMounted(() => {
     loadNote()
     loadCategory()
 })
 
-const loadNote = async() =>{
-    let res = await axios.get("/note/search")
+const loadNote = async () => {
+    let res = await axios.get(`/note/search?page=${pageInfo.page}&pagesize=${pageInfo.pageSize}`)
     let tempRows = res.data.data.rows
-    for(let row of tempRows){
+    for (let row of tempRows) {
         row.content += "..."
         let d = new Date(row.create_time)
-        row.create_time = `${d.getFullYear()}/${d.getMonth()+1}/${d.getDate()}`
+        row.create_time = `${d.getFullYear()}/${d.getMonth() + 1}/${d.getDate()}`
     }
     noteList.value = tempRows;
+    pageInfo.Count = res.data.data.count;
+    pageInfo.pageCount = parseInt(pageInfo.Count / pageInfo.pageSize) + (pageInfo.Count % pageInfo.pageSize > 0 ? 1 : 0)
 }
 
-const loadCategory = async() =>{
+const loadCategory = async () => {
     let res = await axios.get("/category/list")
-    categoryOptions.value = res.data.rows.map((item)=>{
-        return{
-            label:item.name,
-            value:item.id
+    categoryOptions.value = res.data.rows.map((item) => {
+        return {
+            label: item.name,
+            value: item.id
         }
     })
 }
@@ -98,6 +129,11 @@ const add = async () => {
     } else {
         message.error(res.data.msg)
     }
+}
+
+const toPage = async(pageNum)=>{
+    pageInfo.page = pageNum
+    loadNote()
 }
 </script>
 
