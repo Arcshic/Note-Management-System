2d61a701f441bef4910fa8e85000aefc1cf5faf9
Arcshic <20130421042jyl@gmail.com>
2023-10-09 01:21:48 +1300

    finish th update and delete function for category page

--- client/src/main.js
+++ client/src/main.js
@@ -6,10 +6,12 @@ import {createDiscreteApi} from 'naive-ui'
 import {router} from './common/router.js'
 import {createPinia} from "pinia"
 import axios from 'axios'
+import { AdminStore } from './stores/AdminStore'
 
 axios.defaults.baseURL = "http://localhost:8080"
 
 const { message, notification, dialog } = createDiscreteApi(['message','notification', 'dialog' ])
+
 
 const app = createApp(App)
 app.provide("message",message)
@@ -19,4 +21,11 @@ app.provide("dialog",dialog)
 app.use(naive)
 app.use(createPinia())
 app.use(router)
+
+const adminStore = AdminStore()
+axios.interceptors.request.use((config)=>{
+    config.headers.token = adminStore.token
+    return config
+})
+
 app.mount('#app')
--- client/src/views/dashboard/Category.vue
+++ client/src/views/dashboard/Category.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
-        <n-button @click="showAddModel = true">Add</n-button>
+
+            <n-button @click="showAddModel = true">Add Category</n-button>
+
+
         <n-table :bordered="false" :single-line="false">
             <thead>
                 <tr>
@@ -10,28 +13,40 @@
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="(category,index) in categoryList">
+                <tr v-for="(category, index) in categoryList">
                     <td>{{ category.id }}</td>
                     <td>{{ category.name }}</td>
                     <td>
                         <n-space>
-                        <n-button>Update</n-button><n-button>Delete</n-button>
-                    </n-space>
+                            <n-button @click="toUpdate(category)">Update</n-button>
+                            <n-button @click="deleteCategory(category)">Delete</n-button>
+                        </n-space>
                     </td>
                 </tr>
             </tbody>
         </n-table>
         <n-modal v-model:show="showAddModel" preset="dialog" title="Dialog">
-    <template #header>
-      <div>Add a new category</div>
-    </template>
-    <div>
-        <n-input v-model:value="addCategory.name" type = "text" placeholder="Please enter a name for new category" />
-    </div>
-    <template #action>
-      <div><n-button @click="add">Submit</n-button></div>
-    </template>
-  </n-modal>
+            <template #header>
+                <div>Add a new category</div>
+            </template>
+            <div>
+                <n-input v-model:value="addCategory.name" type="text" placeholder="Please enter a name for new category" />
+            </div>
+            <template #action>
+                <div><n-button @click="add">Submit</n-button></div>
+            </template>
+        </n-modal>
+        <n-modal v-model:show="showUpdateModel" preset="dialog" title="Dialog">
+            <template #header>
+                <div>Update a category</div>
+            </template>
+            <div>
+                <n-input v-model:value="updateCategory.name" type="text" placeholder="Please update the category" />
+            </div>
+            <template #action>
+                <div><n-button @click="update">Submit</n-button></div>
+            </template>
+        </n-modal>
     </div>
 </template>
 
@@ -39,9 +54,9 @@
 import { AdminStore } from '../../stores/AdminStore';
 const adminStore = AdminStore()
 
-import { ref, reactive, inject,onMounted } from 'vue'
+import { ref, reactive, inject, onMounted } from 'vue'
 
-import { useRouter,useRoute } from 'vue-router';
+import { useRouter, useRoute } from 'vue-router';
 const router = useRouter();
 const route = useRoute();
 
@@ -52,31 +67,71 @@ const notification = inject("notification")
 const dialog = inject("dialog")
 
 const showAddModel = ref(false)
+const showUpdateModel = ref(false)
 const categoryList = ref([])
 const addCategory = reactive({
-    name:""
+    name: ""
 })
-onMounted(()=>{
+const updateCategory = reactive({
+    id: 0,
+    name: ""
+
+})
+onMounted(() => {
     loadDatas()
 })
 
-const loadDatas = async()=>{
+const loadDatas = async () => {
     let res = await axios.get("/category/list")
     categoryList.value = res.data.rows
 }
 
-const add = async()=>{
-    let res = await axios.post("/category/_token/add",{name:addCategory.name},{headers:{token:adminStore.token}})
-    if(res.data.code ==200){
+const add = async () => {
+    let res = await axios.post("/category/_token/add", { name: addCategory.name })
+    if (res.data.code == 200) {
         loadDatas()
         message.info(res.data.msg)
-    }else{
+    } else {
         message.error(res.data.msg)
     }
     showAddModel.value = false;
 }
+
+const toUpdate = async (category) => {
+    showUpdateModel.value = true;
+    updateCategory.id = category.id
+    updateCategory.name = category.name
+}
+const update = async () => {
+    let res = await axios.put("/category/_token/update", { id: updateCategory.id, name: updateCategory.name })
+    if (res.data.code == 200) {
+        loadDatas()
+        message.info(res.data.msg)
+    } else {
+        message.error(res.data.msg)
+    }
+    showUpdateModel = false;
+}
+const deleteCategory = async (category) => {
+    dialog.warning({
+        title: "Warning",
+        content: "Are you sure you want to delete this category?",
+        positiveText: "Confirm",
+        negativeText: "Cancel",
+        onPositiveClick: async () => {
+            let res = await axios.delete(`/category/_token/delete?id=${category.id}`)
+            if (res.data.code == 200) {
+                loadDatas()
+                message.info(res.data.msg)
+            } else {
+                message.error(res.data.msg)
+            }
+        },
+        onNegativeClick: () => {
+        }
+    });
+
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
