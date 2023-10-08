bf3187f5ab2b4334328b123e882bf89706da5ef7
Arcshic <20130421042jyl@gmail.com>
2023-10-08 18:35:59 +1300

    client end basic build

--- client/src/main.js
+++ client/src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import naive from 'naive-ui'
+import {router} from "./common/router.js"
+import {createPinia} from "pinia"
+import axios from `axios`
 
-createApp(App).mount('#app')
+axios.defaults.baseURL = "http://localhost:8080"
+
+const app = createApp(App)
+app.use(naive)
+app.use(createPinia())
+app.use(router)
+app.mount('#app')
